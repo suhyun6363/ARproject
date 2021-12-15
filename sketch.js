0f140a125ab453c3cebaf84e50b24f2f11b4a601
suhyun__ <90364745+suhyun6363@users.noreply.github.com>
2021-12-15 23:48:50 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,45 @@
-var a;
-var button;
 var img01;
 var img02;
-var video;
-var button;
+var s = 0;
+var counter = 0;
+var timer = 0;
 
-var s;
-var ss;
 
 function preload() {
   img01 = loadImage("pocketmon.jpg");
   img02 = loadImage("ball.png");
 }
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-   
-   button = createButton("START");
-  button.position(50, 30);
-  button.mousePressed(click);
-
-
-  s = 0;
-  ss = 0;
-  a = 0;
- // camera=createCapture(VIDEO);
- // video = createCapture(VIDEO);
- // video.hide();
-  
 }
 
 function draw() {
-  console.log(mouseX / width);
-  background(0, 0, 0);
-  image(img01, 0, 0, width, height);
-
-  rect(0, s, width, height);
-  if (ss === 1) {
-    s = s - 50;
-    a = a + 50;
-    button.position(-500, 30);
-    //image(video,500,500,100,50);
-
-  }
-
-  if (a > 2800) {
-
-    image(img02, 0, height * 0.3, width * 0.35, height * 0.35);
+  if (s = s - 30) {
+    image(img01, 0, 0, width, height);
+      image(img02, 0, height * 0.3, width * 0.35, height * 0.35);
     image(img02, (width * 1) / 3, height * 0.3, width * 0.35, height * 0.35);
     image(img02, (width * 2) / 3, height * 0.3, width * 0.35, height * 0.35);
     fill(200, 200, 200);
     textSize(0.05 * width);
     text("함께 할 포켓몬을 선택해보자!", width * 0.19, height * 0.7);
-  }
-}
-
-function click() {
-  ss = 1;
+    rect(0, s, width, height);
+ 
+   if(counter === 1){
+    timer = timer + 1;
+    console.log(timer);
+  if(timer > 800){
+    window.location.href = "ar.html";
+      			}
+   		}
+	}
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+function mouseClicked() {
+  counter = 1;
+window.location.href = "ar.html";
+}
+
